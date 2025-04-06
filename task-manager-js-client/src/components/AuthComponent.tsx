8cfdbc351095d969634588436d3df0cb4c258422
Dodas <ph005009004@gmail.com>
2025-04-06 18:04:40 -0300

    feat:add some auth component improvements
    Add some style improvements to the authentication form. And add a login form

--- task-manager-js-client/src/components/AuthComponent.tsx
+++ task-manager-js-client/src/components/AuthComponent.tsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import '../styles/Auth.css'
 
 function AuthComponent() {
+    const [action, setAction] = useState("Sign Up")
+
     return(
-        <div className='w-72'>
-            <div className="flex justify-center p-5">
-                <p className='text-3xl'>Sign Up</p>
-            </div>      
+        <div className='w-72 pt-6 pb-6'>
+            <div className="flex justify-center gap-2.5">
+                <p className='text-3xl'>{action}</p>
+            </div>
+            <div className='bg-gray-50 h-0.5 rounded-full w-16 justify-self-center mt-2.5 mb-3'>
+
+            </div>   
             <div className="flex flex-col gap-4">
                 <section>
                     <input className='w-full h-10 p-2.5 border' type="text" name="username" placeholder='Usuário'/>
@@ -14,9 +20,9 @@ function AuthComponent() {
                     <input className="w-full h-10 p-2.5 border" type="password" name="password" placeholder='Senha'/>
                 </section>
                 <section className='flex justify-center'>
-                    <button>Sign Up</button>
+                    <button>{action==="Sign Up"?"Criar":"Entrar"}</button>
                 </section>
-                <p className='self-center'>Possui uma conta? <a href="#">Login</a></p>
+                {action==="Sign Up"? <p className='self-center'>Possui uma conta? <span className='text-blue-500 cursor-pointer' onClick={()=> setAction("Login")}>Entrar</span>.</p>:<p className='self-center'>Não possui uma conta? <span className='text-blue-500 cursor-pointer' onClick={()=> setAction("Sign Up")}>Criar</span>.</p>}
             </div>
         </div>
     );
